37549b3f49c6f12cf3fe504b6b8c20045f4dcd8d
djibomar <djibomar@live.com>
2022-03-04 20:13:19 +0000

    tranforming product name and added min_rpodcuts

--- week3/createContentTrainingData.py
+++ week3/createContentTrainingData.py
@@ -3,10 +3,30 @@ import os
 import random
 import xml.etree.ElementTree as ET
 from pathlib import Path
+import string
+from nltk.tokenize import word_tokenize
+from nltk.stem import SnowballStemmer
+
+stemmer = SnowballStemmer("english")
+
+def clean(text):
+    text = text.lower()
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    return text
+
+def stem(text):
+    tokenize_text = word_tokenize(text)
+    stemmed_text = []
+    
+    for token in tokenize_text :
+        stemmed_word = stemmer.stem(token)
+        stemmed_text.append(stemmed_word)
+    
+    return " ".join(stemmed_text)
 
 def transform_name(product_name):
-    # IMPLEMENT
-    return product_name
+    cleaned_product_name = clean(product_name)
+    return stem(cleaned_product_name)
 
 # Directory for product data
 directory = r'/workspace/search_with_machine_learning_course/data/pruned_products/'
@@ -32,8 +52,34 @@ if os.path.isdir(output_dir) == False:
 if args.input:
     directory = args.input
 # IMPLEMENT:  Track the number of items in each category and only output if above the min
+def count_categories():
+    count_map = {}
+
+    for filename in os.listdir(directory):
+        if filename.endswith(".xml"):
+            print("Processing %s" % filename)
+            f = os.path.join(directory, filename)
+            tree = ET.parse(f)
+            root = tree.getroot()
+            for child in root:
+                # Check to make sure category name is valid
+                if (child.find('name') is not None and child.find('name').text is not None and
+                    child.find('categoryPath') is not None and len(child.find('categoryPath')) > 0 and
+                    child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text is not None):
+                      # Choose last element in categoryPath as the leaf categoryId
+                      cat = child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text
+                      
+                      if not cat in count_map:
+                          count_map[cat] = 0
+                      
+                      count_map[cat] = count_map[cat] + 1
+
+    return count_map
+
 min_products = args.min_products
 sample_rate = args.sample_rate
+
+categories_count = count_categories()
 
 print("Writing results to %s" % output_file)
 with open(output_file, 'w') as output:
@@ -52,6 +98,10 @@ with open(output_file, 'w') as output:
                     child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text is not None):
                       # Choose last element in categoryPath as the leaf categoryId
                       cat = child.find('categoryPath')[len(child.find('categoryPath')) - 1][0].text
+
+                      if categories_count[cat] < min_products:
+                          continue
+
                       # Replace newline chars with spaces so fastText doesn't complain
                       name = child.find('name').text.replace('\n', ' ')
                       output.write("__label__%s %s\n" % (cat, transform_name(name)))
